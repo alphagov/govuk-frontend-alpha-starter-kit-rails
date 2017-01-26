a733f95d0b84a0ed8803c33019ddde3f85940094
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2017-01-26 15:19:51 +0000

    Add example content pages and basic app assets

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,10 @@
+class PageController < ApplicationController
+  def index
+  end
+
+  def page
+    # If a matching page template exists in `app/views/page` it will be rendered
+    # For a a new page at '/page/start' create `start.html.erb` in the above dir
+    render params[:id]
+  end
+end
